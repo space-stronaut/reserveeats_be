0eb2707414d54829bc620f0ab466103bd29f34d5
Ronald Abel <ronald.hermansyah@kinersia.com>
2023-05-06 15:04:41 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,151 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors')
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'ronaldah',
+  password : 'Ronaldah@2023',
+  database: 'flutter_project'
+});
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/reservations', async(req,res) => {
+  try {
+    const hasil = connection.query(
+      'SELECT * FROM reservations',
+      function(err, results, fields) {
+        //console.log(results); // results contains rows returned by server
+        //console.log(fields); // fields contains extra meta data about results, if available
+        res.json({
+          results,
+          // fields
+        })
+      }
+    )
+  } catch (error) {
+    
+  }
+})
+
+app.post('/post/test', async(req,res) => {
+  try {
+    const data = req.body;
+  const query = `INSERT INTO reservations (selectedRoom, selectedPerson, selectedTime) VALUES (?, ?, ?)`;
+  const values = [data.selectedRoom, data.selectedPerson, data.selectedTime];
+
+  connection.query(query, values, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.sendStatus(500);
+      return;
+    }
+
+    console.log(result);
+    res.sendStatus(200);
+  });
+  } catch (error) {
+    
+  }
+})
+
+app.post('/register', async(req,res) => {
+  try {
+    const data = req.body;
+  const query = `INSERT INTO users (username, email, gender, password) VALUES (?, ?, ?, ?)`;
+  const values = [data.username, data.email, data.gender, data.password];
+
+  connection.query(query, values, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.sendStatus(500);
+      return;
+    }
+
+    console.log(result);
+    res.sendStatus(200);
+  });
+  } catch (error) {
+    
+  }
+})
+app.post('/login', async(req,res) => {
+  try {
+    const data = req.body;
+  const query = `select * from users where email = ? and password = ? limit 1`;
+  const values = [data.email, data.password];
+
+  connection.query(query, values, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.sendStatus(500);
+      return;
+    }
+
+    console.log(result);
+    if (result.length > 0) {
+      // res.status(500)
+      res.json({
+        id : result[0].id,
+        email : result[0].email,
+        username : result[0].username
+      }).status(200)
+    }else{
+      res.sendStatus(500)
+    }
+    // res.sendStatus(200);
+  });
+  } catch (error) {
+    
+  }
+})
+
+
+// res.json({
+//   test : "hello"
+// }
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+app.listen(1234)
---
+++ flutter_project.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 06, 2023 at 10:03 AM
+-- Server version: 8.0.32-0ubuntu0.20.04.2
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flutter_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int NOT NULL,
+  `selectedRoom` varchar(255) NOT NULL,
+  `selectedPerson` varchar(255) NOT NULL,
+  `selectedTime` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`id`, `selectedRoom`, `selectedPerson`, `selectedTime`) VALUES
+(2, 'VIP', '4', '15.00'),
+(3, 'Biasa', '3', '12.00'),
+(4, 'VIP', '4', '15.00'),
+(5, 'VIP', '3', '15.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `gender`, `password`) VALUES
+(1, 'andika', 'andika@gmail.com', 'L', 'andika1234'),
+(2, 'Ronald', 'ronald@gmail.com', 'L', 'ronald1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
